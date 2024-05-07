be7efac4a00aa92bb15ad4d3518e7c2561cf6ebf
Wellington2708 <wellingtondevelopercontact@gmail.com>
2024-05-07 20:32:50 +0100

    alterando imagem primeira section

---
+++ index.js
@@ -0,0 +1,4 @@
+const section1 = document.querySelector('.container__imagem');
+console.log(section1)
+section1.src = 'img/Combo.png';
+section1.alt = 'O combo+ é a junção do alura+ e o alura língua'
